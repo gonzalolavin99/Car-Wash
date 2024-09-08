70fdc472c324d3d2238546937f6b3ac6b194a623
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-09-08 01:46:12 -0300

    ADD: Create of the main app in the backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,18 @@
-    
+    import express  from "express";
+    import cors from 'cors';
+    import dotenv from 'dotenv';
+    import bookingRoutes from './routes/bookingRoutes.ts';
+
+    dotenv.config();
+
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    app.use(cors());
+    app.use(express.json());
+
+    app.use('/api/bookings', bookingRoutes);
+
+    app.listen(port, ()=> {
+        console.log(`Server is running on port ${port}`);
+    });
