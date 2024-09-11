c103b4f0469c2e4f2276b1d242bde0a1fe730fc0
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-09-11 00:20:12 -0300

    ADD: authRoutes created

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,27 @@
-    import express  from "express";
-    import cors from 'cors';
-    import dotenv from 'dotenv';
-    import bookingRoutes from './routes/bookingRoutes';
+import express from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes';
+import userRoutes from './routes/userRoutes';
+import bookingRoutes from './routes/bookingRoutes';
 
-    dotenv.config();
+dotenv.config();
 
-    const app = express();
-    const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-    app.use(cors());
-    app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-    app.use('/api/bookings', bookingRoutes);
+// Rutas de autenticación
+app.use('/api/auth', authRoutes);
 
-    app.listen(port, ()=> {
-        console.log(`Server is running on port ${port}`);
-    });
+// Rutas de usuario
+app.use('/api/users', userRoutes);
+
+// Rutas de reservas
+app.use('/api/bookings', bookingRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,2 +1,10 @@
 import express from 'express';
-import {register, login} from '../controllers/'
+import {register, login} from '../controllers/authController'
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+export default router;
+
